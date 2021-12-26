050db04dc61408625983c3dcd08ae01e992561f7
Mike Bell <mike@mercuna.com>
2021-12-26 22:16:18 +0000

    Read D pad and move a small ball around

--- pstest/pstest.c
+++ pstest/pstest.c
@@ -12,28 +12,61 @@ ST7789 st;
 #define DISPLAY_ROWS 240
 #define DISPLAY_COLS 240
 
-#define ST_DATA_LEN (32 + ((DISPLAY_COLS + 1) >> 1))
-
 int main()
 {
   stdio_init_all();
 
-  gpio_init(PICOSYSTEM_LED_G_PIN);
-  gpio_init(PICOSYSTEM_LED_R_PIN);
-  gpio_init(PICOSYSTEM_LED_B_PIN);
-  gpio_set_dir(PICOSYSTEM_LED_R_PIN, GPIO_OUT);
-  gpio_set_dir(PICOSYSTEM_LED_G_PIN, GPIO_OUT);
-  gpio_set_dir(PICOSYSTEM_LED_B_PIN, GPIO_OUT);
-  gpio_put(PICOSYSTEM_LED_G_PIN, 1);
-  gpio_put(PICOSYSTEM_LED_R_PIN, 0);
-  gpio_put(PICOSYSTEM_LED_B_PIN, 0);
+  gpio_init_mask(0xffe100);
+  gpio_set_dir_in_masked(0xff0100);
+  for (uint32_t i = 0; i < 24; i++)
+  {
+    if ((1 << i) & 0xff0100) gpio_pull_up(i);
+  }
 
   st = st7789_init(pio0, 0);
 
-  for (uint32_t i = 0; ; ++i)
+  st7789_start_pixels(&st, DISPLAY_ROWS * DISPLAY_COLS);
+  st7789_dma_repeat_pixel_one_channel(&st, 0, DISPLAY_ROWS * DISPLAY_COLS);
+
+  uint8_t posx = 117;
+  uint8_t posy = 117;
+  const uint8_t width = 4;
+  const uint16_t colour = 0xf800;
+  st7789_start_pixels_at(&st, posx, posy, width, width);
+  st7789_dma_repeat_pixel_one_channel(&st, colour, width * width);
+
+  while (1)
   {
-    st7789_start_pixels_at(&st, 60 * (i & 3), 30 * ((i & 0x1f) >> 2), 60, 30);
-    st7789_dma_repeat_pixel_one_channel(&st, i, 60 * 30);
+    absolute_time_t start_time = get_absolute_time();
+
+    uint8_t newposx = posx;
+    uint8_t newposy = posy;
+
+    if (gpio_get(PICOSYSTEM_SW_UP_PIN) == 0) newposy--;
+    else if (gpio_get(PICOSYSTEM_SW_DOWN_PIN) == 0) newposy++;
+    if (gpio_get(PICOSYSTEM_SW_LEFT_PIN) == 0) newposx--;
+    else if (gpio_get(PICOSYSTEM_SW_RIGHT_PIN) == 0) newposx++;
+
+    if (newposx != posx || newposy != posy)
+    {
+      st7789_start_pixels_at(&st, posx, posy, width, width);
+      st7789_dma_repeat_pixel_one_channel(&st, 0, width * width);
+      st7789_start_pixels_at(&st, newposx, newposy, width, width);
+      st7789_dma_repeat_pixel_one_channel(&st, 0xf800, width * width);
+
+      posx = newposx;
+      posy = newposy;
+    }
+
+    uint32_t frame_time = absolute_time_diff_us(start_time, get_absolute_time());
+
+    int delay = 20000;
+    delay -= absolute_time_diff_us(start_time, get_absolute_time());
+    if (delay > 0)
+      sleep_us(delay);
+
+    start_time = get_absolute_time();
+
   }
 
   return 0;
